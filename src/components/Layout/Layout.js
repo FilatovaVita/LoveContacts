2b5641f1a0011d2e1de55c44bdf719e6cb52091b
Vita Filatova <filatova.vitta@gmail.com>
2023-03-10 21:20:05 +0200

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,15 +3,19 @@ import { Toaster } from 'react-hot-toast';
 import { AppBar } from '../AppBar/AppBar';
 import { Suspense } from 'react';
 import { Container } from '../App/App.styled';
+import { Footer } from '../Footer/Footer';
 
 export const Layout = () => {
   return (
-    <Container>
-      <AppBar />
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
+    <>
+      <Container>
+        <AppBar />
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </Container>
       <Toaster position="top-center" reverseOrder={false} />
-    </Container>
+      <Footer />
+    </>
   );
 };
