1a68b63652eef15d40befb3a61f8c10fee3baa3f
Vita Filatova <filatova.vitta@gmail.com>
2023-03-10 18:22:28 +0200

    Update RestrictedRoute.js

--- src/components/RestrictedRoute.js
+++ src/components/RestrictedRoute.js
@@ -1,6 +1,5 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
-import toast from 'react-hot-toast';
 
 export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn } = useAuth();
